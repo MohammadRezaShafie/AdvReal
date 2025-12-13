f7b26027984bbbacc8366de099cc491dde6ab18a
mr <mr@>
2025-12-13 04:34:59 +0000

    remove prints (previous commit was add api yolov5)

--- detlib/HHDet/yolov11/api.py
+++ detlib/HHDet/yolov11/api.py
@@ -46,9 +46,7 @@ class HHYolov11(DetectorBase):
 
     def __call__(self, batch_tensor, **kwargs):
         
-        print(f"batch_tensor Shape: {batch_tensor.shape}")
         detections_with_grad = self.detector(batch_tensor, augment=False, visualize=False)[0]
-        print(f"detections_with_grad Shape: {detections_with_grad.shape}")
         detections_with_grad_clone = detections_with_grad.clone()
         preds = non_max_suppression(detections_with_grad_clone, self.conf_thres, self.iou_thres)
         cls_max_ids = None
